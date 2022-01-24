3e3ac3e44eb2a777cdc74bcb85e85a660609edcc
Mohammed Saif <mdsaif4online@gmail.com>
2022-01-24 19:52:16 +0530

    7-NPM NodePackageMAnager

---
+++ 7-NPM/7-NodePackageManager.js
@@ -0,0 +1,18 @@
+console.log("NPM-Node Package Manager by CWH");
+
+/*
+npm init -> creates package.json file with all the data we have provided in terminal
+npm install slugify
+npm install -> If node modules is deleted
+npm install slugify@1.3.5 -> To install specific version
+Here: 1.3.5 means, 1-> Major Update 3-> Minor Update 5-> Updating patches
+
+Installing nodemon DevDependencinces only for development purpose
+npm intall nodemon --save-dev
+npm install nodemon --global
+
+To check Version of modules
+npm view slugify version
+npm view nodemon version
+
+*/
